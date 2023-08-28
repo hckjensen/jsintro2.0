fb1bf5a40c42cf6a75926a7004507e60f2dcb98c
hckjensen <hckjensen96@gmail.com>
2023-08-28 13:26:42 +0200

    1.

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let navn = 'HC';
+console.log(navn);
+
+
+let tal1 = 5;
+let tal2 = 10;
+let sum = tal1 + tal2;
+console.log(sum);
+
+let alder = 26;
+alder = alder + 5;
+console.log(alder);
+
+let myNumber = 1;
+myNumber++;
+console.log(myNumber);
+
+myNumber--;
+console.log(myNumber);
